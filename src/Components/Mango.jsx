02f7b729e580a2aecdd98aa763da05d6070d8d74
Ayush Dhamecha <ayuahjiteshdhamecha@gmail.com>
2025-02-18 23:39:14 +0530

    maybe done

---
+++ src/Components/Mango.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Mango(){
+    return (
+        <div>
+        <h1>Mango</h1>
+        </div>
+        )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'; // Your main App component
+import Apple from './Components/Apple'; // Import Apple component (apple.jsx)
+import Mango from './Components/Mango'; // Import Mango component (mango.jsx)
 
+// Render the app with routing
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} /> {/* Default route */}
+        <Route path="/apple" element={<Apple />} /> {/* Apple route */}
+        <Route path="/mango" element={<Mango />} /> {/* Mango route */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
